39d53d9b8aaab9d3aabecf845d24542a05fd6ea6
feleex <1106367854@qq.com>
2021-04-20 18:10:47 +0800

    使用ServiceLoader测试spi

---
+++ src/com/felix/learndemo/DBSearch.java
@@ -0,0 +1,8 @@
+package com.felix.learndemo;
+
+/**
+ * @author felix
+ * @date 2021-04-20 10:18
+ */
+public class DBSearch {
+}
---
+++ src/com/felix/learndemo/FileSearch.java
@@ -0,0 +1,8 @@
+package com.felix.learndemo;
+
+/**
+ * @author felix
+ * @date 2021-04-20 10:17
+ */
+public class FileSearch {
+}
